8d643f9fe669fd03f08659e12598975bee0fe1dd
Ryan McCartan <ryanjmccartan@gmail.com>
2020-03-23 20:52:46 -0500

    hotel is populating in template, working on adding new templates

--- src/components/MessageTemplate.js
+++ src/components/MessageTemplate.js
@@ -5,15 +5,28 @@ import templates from '../data/templates.json';
 
 class MessageTemplate extends Component {
 
-    state = {
+    constructor(props){
+    super(props)
+    this.greeting = this.changeGreeting();
+    this.state = {
         template: {
             id: 1
         },
-        id: 1
+        company: {
+            id: 1
+        },
+        guest: {
+            id: 1
+        },
+        new_message: {
+            id: 1,
+            name: '',
+            message: ''
+        }
     }
+}
 
-
-    getTemplate = () => {
+    getTemplates = () => {
         templates.map(template => {
             this.setState({
                 greeting: template.greeting,
@@ -38,13 +51,24 @@ class MessageTemplate extends Component {
             })
                 // console.log(time.startTimestamp);
         // })
-        console.log('state id', this.state.id);
-        console.log('template id', this.state.template)
+        console.log('state id', this.state.guest.id);
+        console.log('company id', this.state.company.id)
+        console.log('new message id', this.state.new_message.id)
     }
 
     changePerson = (e) => {
         this.setState({
-            id: e.target.value
+            guest: {
+                id: e.target.value
+            }
+        })
+    }
+    
+    changeCompany = (e) => {
+        this.setState({
+            company: {
+                id: e.target.value
+            }
         })
     }
 
@@ -56,59 +80,107 @@ class MessageTemplate extends Component {
         })
     }
 
+    handleNewTemplate = (e) => {
+        this.setState({
+            new_message: {
+                id: templates.length + 1,
+                name: e.target.value,
+                message: e.target.value
+            }
+        })
+    }
+
+    addTemplate = () => {
+        templates.push(this.state.new_message);
+        this.setState({
+            new_message: {
+                id: templates.length,
+                name: '',
+                message: ''
+            }
+        })
+    }
+
+    // checkForVar = (message, ) => {
+    //     for(i = 0; i < message.length; i++){
+    //         if(message[i] == )
+    //     }
+    // }
+
+        changeGreeting = () => {
+            let greeting;
+            const time = new Date().getHours();
+            if(time < 12){
+                greeting = "Good morning"
+            }else if(time < 16){
+                greeting = "Good afternoon"
+            }else{
+                greeting = "Good evening"
+        }
+        return greeting;
+    }
+
     render() {
 
         let firstName;
         let roomNumber;
-        let company = this.state.company; 
+        let hotel; 
         let message;
         
-            guests.map(guest => {
-                if(guest.id == this.state.id){
+        guests.map(guest => {
+            if(guest.id == this.state.guest.id){
                 firstName = guest.firstName;
                 roomNumber = guest.reservation.roomNumber;
-                return;
-                }
+            }
+            return;
         })
 
-       templates.map(template => {
+        companies.map(company => {
+            if(company.id == this.state.company.id){
+                hotel = company.company
+            }
+            return;
+        })
+
+        templates.map(template => {
            if(template.id == this.state.template.id){
                 message = template.message;
-                template.message.replace(/firstName/, firstName)
+                message = message
+                .replace(/#greeting/, this.greeting)
+                .replace(/#firstName/, firstName)
+                .replace(/#company/, hotel)
+                .replace(/#roomNumber/, roomNumber)
                }
             return message;
            }
-       )
-        // let roomNumber = guests.map(guest => {
-        //     Object.keys(guest.reservation).map(key => {
-        //         console.log(guest.reservation[key])
-        //         // return key.roomNumber;
-        //     })
-        //   }
-        // )
+        )
         
         return(
-            <>
-            <select value={this.state.id} onChange={this.changePerson}>{guests.map(guest => {
-                return <option key={guest.id} value={guest.id}>{guest.firstName}</option>
-            })}
-            </select>
-             <select value={this.state.template.id} onChange={this.changeTemplate}>{templates.map(template => {
-                return <option key={template.id} value={template.id}>{template.name}</option>
-            })}
+            <div>
+                <select value={this.state.guest.id} onChange={this.changePerson}>{guests.map(guest => {
+                    return <option key={guest.id} value={guest.id}>{guest.firstName}</option>
+                })}
                 </select>
-            <p>This is message template</p>
-            <button onClick={this.checkData}>Check data</button>
-            <button onClick={this.getTemplate}>Get template</button>
-            {/* {guests.map(guest => {
-                if(guest.id == this.state.id){
-                    return `Good morning ${guest.firstName}, and welcome to Hotel California. Room ${guest.reservation.roomNumber}`
-                }
-            })} */}
-            <h1>{firstName}</h1>
-            <h1>{roomNumber}</h1>
-            <p>{message}</p>
-            </>
+                <select value={this.state.company.id} onChange={this.changeCompany}>{companies.map(company => {
+                    return <option key={company.id} value={company.id}>{company.company}</option>
+                })}
+                </select>
+                <select value={this.state.template.id} onChange={this.changeTemplate}>{templates.map(template => {
+                    return <option key={template.id} value={template.id}>{template.name}</option>
+                })}
+                    </select>
+                <p>This is message template</p>
+                <button onClick={this.checkData}>Check data</button>
+                <button onClick={this.getTemplates}>Get template</button>
+                {/* {guests.map(guest => {
+                    if(guest.id == this.state.id){
+                        return `Good morning ${guest.firstName}, and welcome to Hotel California. Room ${guest.reservation.roomNumber}`
+                    }
+                })} */}
+                <h1>{firstName}</h1>
+                <h1>{roomNumber}</h1>
+                <p>{message}</p>
+            </div>
         )
     }
 }
